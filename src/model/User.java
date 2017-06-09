2ec0da1a2ad28ef57a33d8257cc0b4b16efb9e51
audrynyonata <audrynyonata@gmail.com>
2017-06-10 02:10:48 +0700

    fixing user data member

--- src/model/User.java
+++ src/model/User.java
@@ -9,17 +9,24 @@ public class User {
   private String username;
   private String email;
   private String namaPengguna;
+  private Integer nRepo;
+  private Integer nFollower;
+  
 
   public User(){
     username = "";
     email = "";
     namaPengguna = "";
+    nRepo = 0;
+    nFollower = 0;
   }
   
-  public User (String u, String e, String n){
+  public User (String u, String e, String n, Integer r, Integer f){
     username = u;
     email = e;
     namaPengguna = n;
+    nRepo = r;
+    nFollower = f;
   }
   
   public String getUsername(){
@@ -34,9 +41,20 @@ public class User {
     return namaPengguna;
   }
   
-  public void setData(String u, String e, String n){
+  public Integer getNRepo(){
+    return nRepo;
+  }
+
+  public Integer getNFollower(){
+    return nFollower;
+  }
+
+  
+  public void setData(String u, String e, String n, Integer r, Integer f){
     username = u;
     email = e;
     namaPengguna = n;
+    nRepo = r;
+    nFollower = f;
   }
 }
